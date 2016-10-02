b28e6bfe0b4b27679258fe0c69b93c25395b0ac9
Ryan Flach <ryanflach@users.noreply.github.com>
2016-10-02 11:05:48 -0600

    Alternate completion of tuple exercise with .min() and .max()

--- MinMax.playground/Contents.swift
+++ MinMax.playground/Contents.swift
@@ -3,9 +3,7 @@
 import UIKit
 
 func calculateMinMax(ints: Array<Int>) -> (min: Int, max: Int) {
-    let sorted = ints.sorted()
-    
-    return (sorted.first!, sorted.last!)
+    return (ints.min()!, ints.max()!)
 }
 
 print(calculateMinMax(ints: [4, 1, 2, 5]))
